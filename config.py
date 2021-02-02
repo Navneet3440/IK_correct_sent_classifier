609f6df193b0bda4779f6a7cbbde09ba175912fa
Navneet3440 <navneetmec3440@gmail.com>
2021-02-03 00:09:34 +0530

    Chages2

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ VALID_BATCH_SIZE = 4
 EPOCHS = 20
 BERT_PATH = "bert-base-uncased"
 MODEL_PATH = "./inputs/model.bin"
-TRAINING_FILE = "./inputs/final_training_data_5"
+TRAINING_FILE = "./inputs/final_training_data_5.csv"
 TOKENIZER = transformers.BertTokenizer.from_pretrained(BERT_PATH, do_lower_case=True)
 LINEAR_INPUT_SIZE = 768
