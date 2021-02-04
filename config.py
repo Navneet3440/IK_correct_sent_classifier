1f2baae6e17da0c71f5cf1521498773cc7e5d53b
Navneet3440 <navneetmec3440@gmail.com>
2021-02-04 20:46:42 +0530

    update config file

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ TRAIN_BATCH_SIZE = 8
 VALID_BATCH_SIZE = 4
 EPOCHS = 20
 BERT_PATH = "bert-base-uncased"
-MODEL_PATH = "./inputs/model.bin"
-TRAINING_FILE = "./inputs/final_training_data_5.csv"
+DATASET_FILE_COUNT = 5
+MODEL_PATH = f"./inputs/{BERT_PATH}_{MAX_LEN}_ds{DATASET_FILE_COUNT}_bvl.bin"
+TRAINING_FILE = f"./inputs/final_training_data_{DATASET_FILE_COUNT}.csv"
 TOKENIZER = transformers.BertTokenizer.from_pretrained(BERT_PATH, do_lower_case=True)
 LINEAR_INPUT_SIZE = 768
