347260f661b20e3dc164f89a2c51230288cdad03
Navneet3440 <navneetmec3440@gmail.com>
2021-02-05 10:19:43 +0000

    update git ignore

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ MAX_LEN = 128
 TRAIN_BATCH_SIZE = 8
 VALID_BATCH_SIZE = 4
 EPOCHS = 20
-BERT_PATH = "bert-base-uncased"
+BERT_PATH = "bert-base-cased"
 DATASET_FILE_COUNT = 5
 MODEL_PATH = f"./inputs/{BERT_PATH}_{MAX_LEN}_ds{DATASET_FILE_COUNT}_bvl.bin"
 TRAINING_FILE = f"./inputs/final_training_data_{DATASET_FILE_COUNT}.csv"
-TOKENIZER = transformers.BertTokenizer.from_pretrained(BERT_PATH, do_lower_case=True)
+TOKENIZER = transformers.BertTokenizer.from_pretrained(BERT_PATH, do_lower_case=False)
 LINEAR_INPUT_SIZE = 768
